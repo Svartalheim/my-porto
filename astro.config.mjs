71b55dc505add5e09ca4d7bfabfe708c386d80f2
aqsyalraihanjamil <aqsyal_raihan_28rpl@student.smktelkom-mlg.sch.id>
2025-08-11 22:38:14 +0700

    change to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
