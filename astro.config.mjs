b6128157f39ff37fd4d63adc5576ca573feecbf4
svartalheim <gboomers333@gmail.com>
2025-08-12 13:55:14 +0700

    feat(bottom-bar):overlay

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,29 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import viteCompression from 'vite-plugin-compression';
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [
+      tailwindcss(),
+      viteCompression({
+        algorithm: 'brotliCompress',
+        ext: '.br',
+        deleteOriginFile: false,
+        threshold: 1024, // only compress >1KB
+      }),
+      viteCompression({
+        algorithm: 'gzip',
+        ext: '.gz',
+        deleteOriginFile: false,
+        threshold: 1024,
+      }),
+    ],
+  },
 });
